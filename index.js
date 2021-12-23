f498114f626e3979ce810d71d70f5fb03ac8601a
Alexandre Bekor <contato@alexandrebekor.com>
2021-12-23 15:34:36 -0300

    added connection to database

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+// import server
 const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
 
+// import routes
 const routePages = require('./routes/pages')
 const routeUsers = require('./routes/users')
 
+// import database
+const { Sequelize } = require('sequelize')
+
+// SERVER
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -16,8 +22,25 @@ app.set('views', path.join(__dirname, 'views'))
 // assets
 app.use(express.static(path.join(__dirname, 'public')))
 
-// routes
+// ROUTES
 app.use('/', routePages)
 app.use('/users', routeUsers)
 
-app.listen(port, console.log('Server is runnig'))
+// DATABASE
+const sequelize = new Sequelize('poc-sequelize', 'postgres', 'admin', {
+    host: 'localhost',
+    port: '5433',
+    dialect: 'postgres'
+})
+
+const connect = async () => {
+    try {
+        await sequelize.authenticate()
+            .then(app.listen(port))
+        console.log('connected')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+connect()
